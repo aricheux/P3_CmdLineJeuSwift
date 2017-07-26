a14de563f61ffb782cb98f381072300da96904d0
Richeux Antoine <richeux.antoin44@gmail.com>
2017-07-26 16:02:46 +0200

    Initial Commit

---
+++ P3_CmdLineGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  P3_CmdLineGame
+//
+//  Created by RICHEUX Antoine on 26/07/2017.
+//  Copyright © 2017 Richeux Antoine. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
