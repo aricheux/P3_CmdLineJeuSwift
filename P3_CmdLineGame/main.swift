40163ec12da29551f5ad8786ad66adcf836b7b70
Richeux Antoine <richeux.antoin44@gmail.com>
2017-07-27 10:38:13 +0200

    - ajout des fonctions pour le choix du type et du nom du personnage

--- P3_CmdLineGame/main.swift
+++ P3_CmdLineGame/main.swift
@@ -8,61 +8,15 @@
 import Foundation
 
 // ENUMERATION
-enum characterType {
-    case fighter
-    case mage
-    case colossus
-    case dwarf
+enum characterType : Int {
+    case fighter, mage, colossus, dwarf
 }
 
-enum weaponType{
-    case attack
-    case heal
+enum weaponType : Int{
+    case attack, heal
 }
 
 // CLASSE
-class Player{
-    var name : String
-    var characters : [Character] = []
-    let charactersMax:Int = 3
-    
-    init(name : String){
-        self.name = name
-    }
-    
-    func choiceCharacter() {
-        print("A toi \(self.name)!")
-        for _ in 0...charactersMax-1{
-            // Wait the input value
-            if let response = readLine(){
-                let numType = Int(response)!
-                // Add a new character according to the input value
-                switch numType {
-                case 1:
-                    self.characters.append(Character(type: .fighter, name: "Warrior"))
-                case 2:
-                    self.characters.append(Character(type: .mage, name: "Heal"))
-                case 3:
-                    self.characters.append(Character(type: .colossus, name: "Tank"))
-                case 4:
-                    self.characters.append(Character(type: .dwarf, name: "Nain"))
-                default:
-                    print("Entrée non valide")
-                }
-            }
-
-        }
-    }
-    
-    func showTeamStatus() {
-        print("Equipe de \(self.name) : ")
-        for i in 0...self.characters.count-1{
-            print("\(i+1).\(self.characters[i].type) (vie:\(self.characters[i].life) / dégat:\(self.characters[i].weapon.damage))")
-        }
-        
-    }
-}
-
 class Character {
     var name : String
     var life : Int
@@ -103,14 +57,98 @@ class Weapon{
     }
 }
 
+class Player{
+    var name : String
+    var characters : [Character] = []
+    let charactersMax:Int = 3
+    
+    init(name : String){
+        self.name = name
+    }
+    
+    public func choiceCharacter() {
+        print("********** A toi \(self.name), choisit ton équipe **********")
+        var iCharacters = 0
+        //
+        while (iCharacters < charactersMax) {
+            // Choice the type of the character
+            print("Type du personnage \(iCharacters+1) : 1. Combattant 2. Mage 3. Colosse 4. Nain")
+            if choiceCharacterType(){
+                // Choice the type of the character
+                print("Entrer le nom du personnage \(iCharacters+1) :")
+                if choiceCharacterName(){
+                    // Next character
+                    iCharacters += 1
+                }
+            }
+        }
+    }
+    
+    public func showTeamStatus() {
+        print("Equipe de \(self.name) : ")
+        for i in 0...self.characters.count-1{
+            print("\(i+1).\(self.characters[i].type) (vie:\(self.characters[i].life) / dégat:\(self.characters[i].weapon.damage))")
+        }
+        print("\n")
+    }
+    
+    private func choiceCharacterType() -> Bool{
+        var characterTypeOk = false
+        // Wait the entered value
+        if let response = readLine(){
+            // Check if the input is an integer valude
+            if isStringAnInt(string: response){
+                // convert to integer value
+                let numType = Int(response)!
+                // Add a new character according to the input value
+                switch numType {
+                case 1...4:
+                    // Add new character
+                    // subtract 1 to match with the enumeration
+                    self.characters.append(Character(type: characterType(rawValue: numType-1)!, name: "Warrior"))
+                    // function ok
+                    characterTypeOk = true
+                default:
+                    print("Veuillez entrer un numéro valide")
+                }
+            }else{
+                print("Veuillez entrer un numéro valide")
+            }
+        }
+        
+        return characterTypeOk
+    }
+    
+    private func choiceCharacterName() -> Bool{
+        var characterNameOk = false
+        
+        while characterNameOk == false{
+            // Wait the entered value
+            if let response = readLine(){
+                if response.characters.count > 0{
+                    // Change the character name by the name entered
+                    self.characters[self.characters.count-1].name = response
+                    // function Ok
+                    characterNameOk = true
+                }else{
+                    print("Veuillez entrer un nom valide (minimum 1 charactères)")
+                }
+            }
+        }
+        return characterNameOk
+    }
+    
+    private func isStringAnInt(string: String) -> Bool {
+        return Int(string) != nil
+    }
+}
+
 let player_1 = Player(name: "Antoine")
 let player_2 = Player(name: "Kevin")
 
-print("Choisir le type de vos personnages :"
-    + "\n1. Combattant 2. Mage 3. Colosse 4. Nain")
 //
 player_1.choiceCharacter()
 player_1.showTeamStatus()
 //
 player_2.choiceCharacter()
-player_2.showTeamStatus()
+//player_2.showTeamStatus()
